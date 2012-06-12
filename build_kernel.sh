23ddb9c1efc6551c2dad4816ddd5bd0b5296905f
Ron <cips173@gmail.com>
2012-06-12 10:55:35 +0530

    some change

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,7 +7,7 @@ function setenv {
 	export USE_CCACHE=1
 
 	#THIS LINE SHOULD REFLECT YOUR PATH TO THE TOOLCHAIN!
-	export CROSS_COMPILE=../prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- "
+	export CROSS_COMPILE=../prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
 	#export CFLAGS="-Ofast -mtune=arm1136jf-s -march=armv6j -floop-interchange -floop-strip-mine -fomit-frame-pointer -floop-block -mfpu=vfp -pipe"
 	echo " done."
 
