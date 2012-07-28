35bca0412335f1c70adbf2890271fc621d889388
Ron <cips173@gmail.com>
2012-07-28 13:54:11 +0530

    fix build

--- kernel/sched_autogroup.h
+++ kernel/sched_autogroup.h
@@ -3,6 +3,8 @@
 static inline struct task_group *
 autogroup_task_group(struct task_struct *p, struct task_group *tg);
 
+static inline void autogroup_free(struct task_group *tg) { }
+
 #else /* !CONFIG_SCHED_AUTOGROUP */
 
 static inline void autogroup_init(struct task_struct *init_task) {  }
